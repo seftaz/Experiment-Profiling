c7b75bd0d0ba9c0a39076221053346e87d8fbbf8
Arad Maleki <arad.m.t02@gmail.com>
2024-05-03 23:54:59 +0330

    Modified temp method

---
+++ First Section/src/JavaCup.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class JavaCup {
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Press number1: ");
+        int i = scanner.nextInt();
+        System.out.println("Press number2: ");
+        int j = scanner.nextInt();
+        System.out.println("Press number3: ");
+        int k = scanner.nextInt();
+        temp();
+        eval(i, j, k);
+    }
+    public static void eval(int i, int j, int k)
+    {
+        if (i * i + j * j == k * k || i * i == j * j + k * k || j * j == i * i + k * k)
+        {
+            System.out.println("YES");
+        }
+        else { System.out.println("NO"); }
+    }
+    public static void temp() {
+        return;
+    }
+}
